dd70f67f6b41ff1334eca3713b5a856f2c355956
rakesh dyavath <davathrak@gmail.com>
2022-07-10 20:49:53 +0530

    added user

--- halls/views.py
+++ halls/views.py
@@ -1,9 +1,10 @@
 from django.http import HttpRequest
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.urls import reverse_lazy
 
 from django.views import generic
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import  authenticate , login
 from .models import Hall , Video
 
 
@@ -18,14 +19,26 @@ class SignUp(generic.CreateView):
     success_url = reverse_lazy('home')
     template_name = 'registration/signup.html'
 
+    def form_valid(self, form):
+        view = super(SignUp, self).form_valid(form)
+        username  = form.cleaned_data.get('username')
+        password = form.cleaned_data.get('password1')
+        user = authenticate(username=username, password=password)
+        login(self.request, user)
+        return view
 
 #to create hall using class based views 
 
 class Create_view(generic.CreateView):
     model = Hall
     template_name = "halls/create.html"
-    fields = "__all__"
+    fields = ['title']
     success_url = reverse_lazy('home')
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        super(Create_view, self).form_valid(form)
+        return redirect('home')
 
 
     
